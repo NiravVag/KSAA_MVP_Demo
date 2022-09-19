ed4ae6574149219accf258dc81f0d7327996c795
Nirav Vaghela <nirav.softvanlabs@gmail.com>
2022-09-19 20:38:58 +0530

    Commit_SmallChanges_Done_19-09-22

--- Web/UserInterface/Models/Account/LoginViewModel.cs
+++ Web/UserInterface/Models/Account/LoginViewModel.cs
@@ -20,7 +20,7 @@ namespace KSAA.UserInterface.Web.Models.Account
         public string Id { get; set; }
         public string FirstName { get; set; }
         public string LastName { get; set; }
-        public string UserStatus { get; set; }
+        //public string UserStatus { get; set; }
         public string Email { get; set; }
         public string Token { get; set; }
         public string RefreshToken { get; set; }
