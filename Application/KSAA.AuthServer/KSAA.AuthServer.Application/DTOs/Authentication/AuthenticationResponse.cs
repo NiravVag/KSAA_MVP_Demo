38b9a8edcd661befe4d5bfcf69ccb38a84b35930
Nirav Vaghela <nirav.softvanlabs@gmail.com>
2022-09-19 20:15:48 +0530

    Commit_LoginWithThemeUpdate_Done_19-09-22

--- Application/KSAA.AuthServer/KSAA.AuthServer.Application/DTOs/Authentication/AuthenticationResponse.cs
+++ Application/KSAA.AuthServer/KSAA.AuthServer.Application/DTOs/Authentication/AuthenticationResponse.cs
@@ -9,7 +9,8 @@ namespace KSAA.AuthServer.ApiServices.Application.DTOs.Authentication
     public class AuthenticationResponse
     {
         public string Id { get; set; }
-        public string UserName { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
         public string UserStatus { get; set; }
         public string Email { get; set; }
         public string Token { get; set; }
